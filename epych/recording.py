7ecbc349a47093986c7adc3dcd8b157a4e339894
Eli Sennesh <eli.sennesh@vanderbilt.edu>
2024-01-18 17:12:31 -0600

    recording.EpochedSeries: replace TimeLockedSeries with an epoched timeseries
    Signed-off-by: Eli Sennesh <eli.sennesh@vanderbilt.edu>

--- epych/recording.py
+++ epych/recording.py
@@ -46,92 +46,82 @@ class ContinuousRecording:
                   if ((v >= first) & (v <= last)).all()}
         return EpochedSeries(events, **signals)
 
-    @property
-    def events(self):
-        return self._events
-
-    def _event_bounds(self, event):
-        event_keys = list(self.events.keys())
-        successor = event_keys[event_keys.index(event) + 1]
-        return self.events[event].values, self.events[successor].values
-
-    def select_trials(self, f, *columns):
-        trial_entries = (list(self.trials[col].values) for col in columns)
-        selections = np.array([f(*entry) for entry in zip(*trial_entries)])
-
-        events = self.events.loc[selections]
-        trials = self.trials.loc[selections]
-        signals = {k: s.select_trials(selections)
-                   for k, s in self.signals.items()}
-        return Recording(events, trials, **signals)
-
-    @property
-    def signals(self):
-        return self._signals
-
-    @property
-    def trials(self):
-        return self._trials
-
-    def time_lock(self, event, duration=True, before=0., after=0.):
-        onsets, offsets = self._event_bounds(event)
-        if not isinstance(duration, bool):
-            offsets = onsets + duration
-        onsets = onsets - before
-        offsets = offsets + after
-        first, last = onsets.min(), offsets.max()
-
-        signals = {k: s[first:last] for k, s in self.signals.items()}
+class EpochedSeries:
+    def __init__(self, trial_info, units, **signals):
         for sig in signals.values():
-            sig.mask_events(onsets, offsets)
-
-        events = {k: v for k, v in self.events.items()
-                  if ((v >= first) & (v <= last)).all()}
-        return TimeLockedSeries(events, **signals)
-
-class TimeLockedSeries:
-    def __init__(self, events, **signals):
-        self._events = events
+            assert isinstance(sig, signal.EpochedSignal)
+            assert sig.num_trials == len(trial_info)
         self._signals = signals
-        self._shape = None
-        for signal in self.signals.values():
-            if self._shape is None:
-                self._shape = signal.data.shape
-            else:
-                assert signal.data.shape == self.shape
-        assert len(self.shape) == 3 # Channels x Times x Trials
+        self._trial_info = trial_info
+        self._units = units
 
     @property
     def events(self):
-        return self._events
+        for column in self.trial_info.columns:
+            if isinstance(self.units[column], pq.UnitTime):
+                yield column
 
-    def plot_trial(self, t=None):
+    def plot(self, trial=None):
         fig, axes = plt.subplot_mosaic([[sig] for sig in self.signals],
                                        layout='constrained', sharex=True)
         for sig, ax in axes.items():
             ax.set_title(sig)
-            if t is not None:
-                signal = self.signals[sig].select_trials([t])
-                signal.plot(ax=ax)
+            if trial is not None:
+                signal = self.signals[sig].select_trials([trial])
             else:
                 signal = self.signals[sig].erp()
-                signal.plot(ax=ax)
+            signal.plot(ax=ax)
 
         for event in self.events:
-            if t is not None:
-                event_time = self.events[event][t]
+            if trial is not None:
+                event_time = self.trial_info[event][trial]
             else:
-                event_time = self.events[event].mean()
+                event_time = self.trial_info[event].mean()
             for ax in axes.values():
                 ymin, ymax = ax.get_ybound()
                 ax.vlines(event_time, ymin, ymax, colors='black',
                           linestyles='dashed', label=event)
                 ax.annotate(event, (event_time + 0.005, ymax))
 
-    @property
-    def shape(self):
-        return self._shape
+    def select_trials(self, f, *columns):
+        trial_entries = (list(self.trial_info[col].values) for col in columns)
+        selections = np.array([f(*entry) for entry in zip(*trial_entries)])
+
+        trial_info = self.trial_info.loc[selections]
+        signals = {k: s.select_trials(selections) for k, s
+                   in self.signals.items()}
+        return EpochedSeries(trial_info, self.units, **signals)
 
     @property
     def signals(self):
         return self._signals
+
+    def time_lock(self, event, before=0., after=0.):
+        event_times = self.trial_info[event]
+        onsets = (event_times - before).to_numpy()
+        offsets = (event_times + after).to_numpy()
+        first, last = onsets.min(), offsets.max()
+
+        signals = {k: s[first:last] for k, s in self.signals.items()}
+        for sig in signals.values():
+            sig.mask_epochs(onsets, offsets)
+
+        columns = []
+        for column in self.trial_info.columns:
+            if isinstance(self.units[column], pq.UnitTime):
+                v = self.trial_info[column]
+                if ((v >= first) & (v <= last)).all():
+                    columns.append(column)
+            else:
+                columns.append(column)
+        trial_info = self.trial_info.filter(items=columns, axis="columns")
+        units = {k: v for k, v in self.units.items() if k in columns}
+        return EpochedSeries(trial_info, units, **signals)
+
+    @property
+    def trial_info(self):
+        return self._trial_info
+
+    @property
+    def units(self):
+        return self._units
