1e80639a0ee06a06fa6bf641807f2f14ffd23618
Eli Sennesh <eli.sennesh@vanderbilt.edu>
2024-03-01 15:13:27 -0600

    statistic.Statistic: abstract base class for across-subject statistics
    Signed-off-by: Eli Sennesh <eli.sennesh@vanderbilt.edu>

---
+++ epych/statistic.py
@@ -0,0 +1,69 @@
+#!/usr/bin/python3
+
+import copy
+import hdf5storage as mat
+import matplotlib.pyplot as plt
+import numpy as np
+import os
+import pandas as pd
+import pickle
+
+from . import signal
+
+class Statistic:
+    def __init__(self, channels, iid_shape, values=None):
+        assert isinstance(channels, pd.DataFrame)
+        stat_shape = (len(channels), *iid_shape)
+        assert values is None or values.shape[:len(stat_shape)] == stat_shape
+
+        self._channels = channels
+        self._vals = values
+
+    @property
+    def channels(self):
+        return self._channels
+
+    def evoked(self):
+        raise NotImplementedError
+
+    def fmap(self, f):
+        return self.__class__(self.channels, self.iid_shape, f(self.values))
+
+    def pickle(self, path):
+        assert os.path.isdir(path) or not os.path.exists(path)
+        os.makedirs(path, exist_ok=True)
+
+        self.channels.to_csv(path + "/channels.csv")
+        arrays = {k: v for k, v in self.__dict__.items()
+                  if isinstance(v, np.ndarray)}
+        mat.savemat(path + ("/%s.mat" % self.__class__.name), arrays)
+        other = copy.copy(self)
+        for k in arrays:
+            setattr(other, k, None)
+        with open(path + ("/%s.pickle" % self.__class__.name), mode="wb") as f:
+            pickle.dump(other, f)
+
+    def select_channels(self, mask):
+        channels = self.channels.loc[mask]
+        return self.fmap(lambda vals: vals[mask, :])
+
+    @classmethod
+    def unpickle(cls, path):
+        assert os.path.isdir(path)
+
+        with open(path + ("/%s.pickle" % self.__class__.name), mode="rb") as f:
+            self = pickle.load(f)
+
+        arrays = mat.loadmat(path + ("/%s.mat" % self.__class__.name))
+        for k in arrays:
+            setattr(self, k, arrays[k])
+        self._channels = pd.read_csv(path + "/channels.csv", index_col=0)
+        self._channels["location"] = self._channels["location"].apply(eval)
+        return self
+
+    def update(self, s: signal.EpochedSignal, **kwargs):
+        raise NotImplementedError
+
+    @property
+    def values(self):
+        return self._vals
