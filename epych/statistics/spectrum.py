67ff1d690fb5eccd42af8f87ea13661a046f633f
Eli Sennesh <eli.sennesh@vanderbilt.edu>
2024-03-01 15:47:38 -0600

    statistics.spectrum.PowerSpectrum: rewrite power spectra as statistics
    Signed-off-by: Eli Sennesh <eli.sennesh@vanderbilt.edu>

--- epych/statistics/spectrum.py
+++ epych/statistics/spectrum.py
@@ -7,38 +7,45 @@ import numpy as np
 import os
 import pandas as pd
 import pickle
+import scipy.fft as fft
 
-from . import plotting
+from .. import plotting, signal, statistic
 
 THETA_BAND = (1., 4.)
 ALPHA_BETA_BAND = (8., 30.)
 GAMMA_BAND = (50., 150.)
 
-class Spectrum:
-    def __init__(self, df, pows, channels):
-        assert isinstance(channels, pd.DataFrame)
-        assert len(channels) == pows.shape[0]
-
-        self._channels = channels
+class PowerSpectrum(statistic.Statistic):
+    def __init__(self, df, channels, f0, values=None):
         self._df = df
-        self._freqs = np.arange(0, pows.shape[1] / df, self.df)[np.newaxis, :]
-        self._pows = pows
+        self._freqs = np.arange(0, f0, df)[np.newaxis, :]
+        super().__init__(channels, (f0,), values=values)
 
     def band_power(self, fbottom, ftop):
         ibot = np.nanargmin((self.freqs - fbottom) ** 2)
         itop = np.nanargmin((self.freqs - ftop) ** 2)
-        return self.pows[:, ibot:itop+1].mean(axis=-1)
-
-    @property
-    def channels(self):
-        return self._channels
+        return self.values[:, ibot:itop+1].mean(axis=1)
 
     def closest_freq(self, f):
         return np.nanargmin((self.freqs - f) ** 2)
 
+    def decibels(self):
+        return self.fmap(lambda vals: 10 * np.log10(vals))
+
     @property
     def df(self):
         return self._df
+
+    def evoked(self):
+        return self.fmap(lambda vals: vals.mean(axis=-1))
+
+    def fmap(self, f):
+        return self.__class__(self.df, self.channels, self.f0,
+                              values=f(self.values))
+
+    @property
+    def f0(self):
+        return self._freqs[:, -1] + self.df
 
     @property
     def freqs(self):
@@ -52,60 +59,39 @@ class Spectrum:
         if ftop is None:
             ftop = self.freqs[0, -1]
 
-        plotting.heatmap(fig, ax, self.pows, "Power Spectral Density", vmin=0.,
-                         vmax=self.pows.max())
+        plotting.heatmap(fig, ax, self.values, "Power Spectral Density",
+                         vmin=0., vmax=self.values.max())
         ax.set_xlim(left=fbottom, right=ftop)
-
-    def decibels(self):
-        return Spectrum(self.df, 10 * np.log10(self.pows), self.channels)
-
-    def pickle(self, path):
-        assert os.path.isdir(path) or not os.path.exists(path)
-        os.makedirs(path, exist_ok=True)
-
-        self.channels.to_csv(path + '/channels.csv')
-        mat.savemat(path + '/power_spectrum.mat', {"freqs": self.freqs,
-                                                   "pows": self.pows})
-        other = copy.copy(self)
-        other._channels = other._freqs = other._pows = None
-        with open(path + "/power_spectrum.pickle", mode="wb") as f:
-            pickle.dump(other, f)
 
     def plot_channels(self, stat, ax=None, xlims=None):
         if ax is None:
             ax = plt.gca()
 
-        channels = np.arange(0, self.pows.shape[0])
+        channels = np.arange(0, self.values.shape[0])
         ax.plot(stat, channels)
         if xlims is not None:
             ax.set_xlim(*xlims)
         ax.invert_yaxis()
 
-    @property
-    def pows(self):
-        return self._pows
-
     def relative(self):
-        max_pow = self.pows.max(axis=0, keepdims=True)
-        return Spectrum(self.df, self.pows / max_pow, self.channels)
+        max_pow = self.values.max(axis=0, keepdims=True)
+        return self.fmap(lambda vals: vals / max_pow)
 
-    def select_channels(self, mask):
-        channels = self.channels.loc[mask] if self.channels is not None else None
-        pows = self.pows[mask, :]
-        return Spectrum(self.df, pows, channels)
+    def update(self, s: signal.EpochedSignal, taper=None):
+        assert (s.channels == self.channels).all().all()
+        assert s.df == self.df
 
-    def trial_mean(self, axis=-1):
-        return Spectrum(self.df, self.pows.mean(axis=axis), self.channels)
+        xs = s.data
+        if taper is not None:
+            xs = taper(xs.shape[1])[np.newaxis, :, np.newaxis] * xs
+        xf = fft.rfft(xs - xs.mean(axis=1, keepdims=True), axis=1)
+        pows = (2 * s.dt ** 2 / s.T) * (xf * xf.conj())
+        pows = pows[:, 0:xs.shape[1] // 2].real
+        assert pows.shape[1] == self.f0
 
-    @classmethod
-    def unpickle(cls, path):
-        assert os.path.isdir(path)
+        if self.values is None:
+            self._vals = pows
+        else:
+            self._vals = np.concatenate((self.values, pows), axis=-1)
 
-        with open(path + "/power_spectrum.pickle", mode="rb") as f:
-            self = pickle.load(f)
-
-        arrays = mat.loadmat(path + "/power_spectrum.mat")
-        self._freqs, self._pows = arrays["freqs"], arrays["pows"]
-        self._channels = pd.read_csv(path + '/channels.csv', index_col=0)
-        self._channels["location"] = self._channels["location"].apply(eval)
-        return self
+        return self.values
