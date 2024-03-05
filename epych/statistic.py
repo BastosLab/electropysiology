98336f7c8a3036ada919c5e31303f81b2f61a020
Eli Sennesh <eli.sennesh@vanderbilt.edu>
2024-03-05 16:03:55 -0600

    statistic.Statistic: remove all channel information from Statistic class
    Signed-off-by: Eli Sennesh <eli.sennesh@vanderbilt.edu>

--- epych/statistic.py
+++ epych/statistic.py
@@ -16,13 +16,9 @@ from . import signal
 T = TypeVar('T')
 
 class Statistic(Generic[T]):
-    def __init__(self, channels: pd.DataFrame, iid_shape,
-                 data: Optional[np.ndarray]=None):
-        assert isinstance(channels, pd.DataFrame)
-        stat_shape = (len(channels), *iid_shape)
-        assert data is None or data.shape[:len(stat_shape)] == stat_shape
-
-        self._channels = channels
+    def __init__(self, iid_shape, data: Optional[np.ndarray]=None):
+        assert data is None or data.shape[:len(iid_shape)] == iid_shape
+        self._iid_shape = iid_shape
         self._data = data
 
     def apply(self, element: tuple[T, ...]) -> np.ndarray:
@@ -34,21 +30,20 @@ class Statistic(Generic[T]):
         return self._data
 
     @property
-    def channels(self):
-        return self._channels
-
-    @property
     def data(self):
         return self._data
 
     def fmap(self, f):
-        return self.__class__(self.channels, self.iid_shape, f(self.values))
+        return self.__class__(self.iid_shape, f(self.values))
+
+    @property
+    def iid_shape(self):
+        return self._iid_shape
 
     def pickle(self, path):
         assert os.path.isdir(path) or not os.path.exists(path)
         os.makedirs(path, exist_ok=True)
 
-        self.channels.to_csv(path + "/channels.csv")
         arrays = {k: v for k, v in self.__dict__.items()
                   if isinstance(v, np.ndarray)}
         mat.savemat(path + ("/%s.mat" % self.__class__.name), arrays)
@@ -57,10 +52,6 @@ class Statistic(Generic[T]):
             setattr(other, k, None)
         with open(path + ("/%s.pickle" % self.__class__.name), mode="wb") as f:
             pickle.dump(other, f)
-
-    def select_channels(self, mask):
-        channels = self.channels.loc[mask]
-        return self.fmap(lambda vals: vals[mask, :])
 
     @classmethod
     def unpickle(cls, path):
@@ -72,6 +63,4 @@ class Statistic(Generic[T]):
         arrays = mat.loadmat(path + ("/%s.mat" % self.__class__.name))
         for k in arrays:
             setattr(self, k, arrays[k])
-        self._channels = pd.read_csv(path + "/channels.csv", index_col=0)
-        self._channels["location"] = self._channels["location"].apply(eval)
         return self
