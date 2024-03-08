7302df675dd584b51b42bd70c0d7ba7c0d090dbd
Eli Sennesh <eli.sennesh@vanderbilt.edu>
2024-03-08 13:56:42 -0600

    statistics.alignment.LaminarAlignment: use relative channel index to align
    Signed-off-by: Eli Sennesh <eli.sennesh@vanderbilt.edu>

--- epych/statistics/alignment.py
+++ epych/statistics/alignment.py
@@ -17,10 +17,14 @@ class LaminarAlignment(statistic.Statistic[signal.EpochedSignal]):
         area_l4 = os.path.commonprefix([l.decode() for l
                                         in element.channels.location]) + "4"
         l4_mask = [area_l4 in loc.decode() for loc in element.channels.location]
-        l4_center = round(np.median(element.channels.loc[l4_mask].index))
 
-        sample = np.array((element.channels.index[0], l4_center,
-                           element.channels.index[-1]))[np.newaxis, :]
+        channels_index = element.channels.channel\
+                         if "channel" in element.channels.columns\
+                         else element.channels.index
+        l4_center = round(np.median(channels_index[l4_mask]))
+
+        sample = np.array((channels_index.values[0], l4_center,
+                           channels_index.values[-1]))[np.newaxis, :]
         if self.data is None:
             return sample
         return np.concatenate((self.data, sample), axis=0)
@@ -28,14 +32,11 @@ class LaminarAlignment(statistic.Statistic[signal.EpochedSignal]):
     def calculate(self, elements: Iterable[signal.Signal]):
         for element in elements:
             self._data = self.apply(element)
-        l4_channel = np.median(self._data[:, 1])
-        superficial_distance = (self._data[:, 0] - l4_channel).mean()
-        deep_distance = (self._data[:, 2] - l4_channel).mean()
-
-        superficial_channel = max(round(l4_channel + superficial_distance), 0)
-        deep_channel = min(round(l4_channel + deep_distance),
-                           max(self._data[:, 2]))
-        return np.array([superficial_channel, l4_channel, deep_channel])
+        l4_channels = self._data[:, 1]
+        superficial_distance = (l4_channels - self._data[:, 0]).mean()
+        deep_distance = (self._data[:, 2] - l4_channels).mean()
+        return np.array([l4_channels - superficial_distance, l4_channels,
+                         l4_channels + deep_distance]).T.round()
 
     def fmap(self, f):
         return self.__class__(self._area, self._column, f(self.data))
