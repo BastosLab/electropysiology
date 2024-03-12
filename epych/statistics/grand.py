aed3bf26126ba12513c538b622ef5fa11ca21ac0
Eli Sennesh <eli.sennesh@vanderbilt.edu>
2024-03-12 14:21:42 -0500

    statistics.grand.GrandAverage: write grand average with data as dict
    Signed-off-by: Eli Sennesh <eli.sennesh@vanderbilt.edu>

--- epych/statistics/grand.py
+++ epych/statistics/grand.py
@@ -4,12 +4,15 @@ import numpy as np
 
 from .. import signal, statistic
 
-class GrandAverage(statistic.ChannelwiseStatistic[signal.EpochedSignal]):
+class GrandAverage(statistic.Statistic[signal.EpochedSignal]):
+    def __init__(self, iid_shape, data=None):
+        if data is None:
+            data = {"n": 0, "sum": np.zeros(*self.iid_shape)}
+        super().__init__(iid_shape, data=data)
+
     def apply(self, element: signal.EpochedSignal):
         assert element.data.shape[:-1] == self.iid_shape
-        running = self._data if self._data is not None else {
-            "n": 0, "sum": np.zeros(*self.iid_shape),
-        }
+        running = self._data.copy()
         running["sum"] += element.data.sum(axis=-1)
         running["n"] += element.num_trials
         return running
