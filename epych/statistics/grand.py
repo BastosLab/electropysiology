f7dbb93f4abb806fe600ded5f8343318d08efdfe
Eli Sennesh <eli.sennesh@vanderbilt.edu>
2024-03-12 15:04:06 -0500

    statistics.grand.GrandAverage: ongoing fixups
    Signed-off-by: Eli Sennesh <eli.sennesh@vanderbilt.edu>

--- epych/statistics/grand.py
+++ epych/statistics/grand.py
@@ -1,19 +1,20 @@
 #!/usr/bin/python3
 
+import copy
 import numpy as np
 
 from .. import signal, statistic
 
 class GrandAverage(statistic.Statistic[signal.EpochedSignal]):
     def __init__(self, iid_shape, data=None):
-        if data is None:
-            data = {"n": 0, "sum": np.zeros(*self.iid_shape)}
         super().__init__(iid_shape, data=data)
+        if data is None:
+            self._data = {"n": 0, "sum": np.zeros((*self.iid_shape, 1))}
 
     def apply(self, element: signal.EpochedSignal):
         assert element.data.shape[:-1] == self.iid_shape
-        running = self._data.copy()
-        running["sum"] += element.data.sum(axis=-1)
+        running = copy.deepcopy(self.data)
+        running["sum"] += element.data.sum(axis=-1, keepdims=True)
         running["n"] += element.num_trials
         return running
 
