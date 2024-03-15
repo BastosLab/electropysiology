40345d4fed9674ea41cff0313558bcaffe0e5094
Eli Sennesh <eli.sennesh@vanderbilt.edu>
2024-03-15 11:08:01 -0500

    statistics.grand.GrandAverage: include plotting method for grand heatmaps
    Signed-off-by: Eli Sennesh <eli.sennesh@vanderbilt.edu>

--- epych/statistics/grand.py
+++ epych/statistics/grand.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 
 import copy
+import matplotlib.pyplot as plt
 import numpy as np
 
-from .. import signal, statistic
+from .. import plotting, signal, statistic
 
 class GrandAverage(statistic.Statistic[signal.EpochedSignal]):
     def __init__(self, iid_shape, data=None):
@@ -23,3 +24,20 @@ class GrandAverage(statistic.Statistic[signal.EpochedSignal]):
 
     def result(self):
         return self.data["sum"] / self.data["n"]
+
+    def heatmap(self, ax=None, fig=None, title=None, vmin=None, vmax=None,
+                origin="lower"):
+        if ax is None:
+            ax = plt.gca()
+        if fig is None:
+            fig = plt.gcf()
+
+        data = self.result().squeeze()
+        plotting.heatmap(fig, ax, data, cbar=False, title=title, vmin=vmin,
+                         vmax=vmax)
+
+        num_xticks = len(ax.get_xticks())
+        xtick_locs = np.linspace(0, data.shape[1], num_xticks)
+        xticks = np.linspace(0, data.shape[-1], num_xticks)
+        xticks = ["%0.2f" % t for t in xticks]
+        ax.set_xticks(xtick_locs, xticks)
