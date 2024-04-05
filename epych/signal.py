147c34443f6771283e6bcd3a5514d074022f79ab
Eli Sennesh <eli.sennesh@vanderbilt.edu>
2024-04-05 13:39:03 -0500

    signal.EvokedSignal: allow for passing in a figure filename to save alone
    Signed-off-by: Eli Sennesh <eli.sennesh@vanderbilt.edu>

--- epych/signal.py
+++ epych/signal.py
@@ -289,18 +289,15 @@ class EvokedSignal(EpochedSignal):
             data = np.where(data < 0., -np.log(-data), np.log(data))
         ax.plot(self.times, data, **kwargs)
 
-    def heatmap(self, ax=None, fig=None, logspace=False, title=None, vmin=None,
+    def heatmap(self, ax=None, fig=None, filename=None, title=None, vmin=None,
                 vmax=None, origin="lower", channel_ticks="location"):
         if ax is None:
             ax = plt.gca()
         if fig is None:
-            fig = plt.gcf()
+            figure = plt.gcf()
 
         data = self.data.squeeze()
-        if logspace:
-            data = np.log(data, where=data > 0.)
-            data = np.where(data < 0., -np.log(-data, where=data <0.), data)
-        plotting.heatmap(fig, ax, data, cbar=(vmin is None and vmax is None),
+        plotting.heatmap(figure, ax, data, cbar=(vmin is None and vmax is None),
                          title=title, vmin=vmin, vmax=vmax)
 
         num_xticks = len(ax.get_xticks())
@@ -311,6 +308,10 @@ class EvokedSignal(EpochedSignal):
 
         if channel_ticks is not None and channel_ticks in self.channels.columns:
             self.annotate_channels(ax, channel_ticks)
+        if filename is not None:
+            figure.savefig(filename)
+            if fig is None:
+                plt.close(figure)
 
     def plot(self, *args, **kwargs):
         return self.line_plot(*args, **kwargs)
