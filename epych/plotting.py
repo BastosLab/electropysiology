c32b6c7c1fbb2bcda26fe35881349af18c4817ac
Eli Sennesh <eli.sennesh@vanderbilt.edu>
2024-07-30 18:40:04 -0500

    epych.plotting: use Parula as default colormap
    Signed-off-by: Eli Sennesh <eli.sennesh@vanderbilt.edu>

--- epych/plotting.py
+++ epych/plotting.py
@@ -3,18 +3,22 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
+from . import colormaps
+
 def extents(f):
     delta = f[1] - f[0]
     return [f[0] - delta/2, f[-1] + delta/2]
 
-def imagesc(ax, cs, alpha=None, **kwargs):
+def imagesc(ax, cs, alpha=None, cmap=None, **kwargs):
+    if cmap is None:
+        cmap = colormaps.parula
     if kwargs['vmin'] is None and kwargs['vmax'] is None:
         std_dev = cs.std()
         kwargs['vmin'] = 2 * -std_dev
         kwargs['vmax'] = 2 * std_dev
     x, y = np.linspace(0, cs.shape[1]), np.linspace(0, cs.shape[0])
     return ax.imshow(cs, alpha=alpha, aspect='auto', interpolation='none',
-                     extent=extents(x) + extents(y), **kwargs)
+                     extent=extents(x) + extents(y), cmap=cmap, **kwargs)
 
 def heatmap(fig, ax, data, alpha=None, title=None, cbar=True, vmin=-1e-4,
             vmax=1e-4):
