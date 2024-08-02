515a088632cc1b9150f4f3e765d85f6607b87ea7
Eli Sennesh <eli.sennesh@vanderbilt.edu>
2024-08-02 12:13:13 -0500

    plotting: smooth heatmaps with Gaussian kernel of width 4
    Signed-off-by: Eli Sennesh <eli.sennesh@vanderbilt.edu>

--- epych/plotting.py
+++ epych/plotting.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 
+import math
 import matplotlib.pyplot as plt
 import numpy as np
+import cv2 as cv
 
 from . import colormaps
 
@@ -9,7 +11,7 @@ def extents(f):
     delta = f[1] - f[0]
     return [f[0] - delta/2, f[-1] + delta/2]
 
-def imagesc(ax, cs, alpha=None, cmap=None, **kwargs):
+def imagesc(ax, cs, alpha=None, cmap=None, smooth=True, **kwargs):
     if cmap is None:
         cmap = colormaps.parula
     if kwargs['vmin'] is None and kwargs['vmax'] is None:
@@ -17,14 +19,19 @@ def imagesc(ax, cs, alpha=None, cmap=None, **kwargs):
         kwargs['vmin'] = 2 * -std_dev
         kwargs['vmax'] = 2 * std_dev
     x, y = np.linspace(0, cs.shape[1]), np.linspace(0, cs.shape[0])
+
+    kernel = np.ones((4, 4))
+    kernel /= math.prod(kernel.shape)
+    cs = cv.filter2D(cs, -1, kernel)
+
     return ax.imshow(cs, alpha=alpha, aspect='auto', interpolation='none',
                      extent=extents(x) + extents(y), cmap=cmap, **kwargs)
 
 def heatmap(fig, ax, data, alpha=None, title=None, cbar=True, vmin=-1e-4,
-            vmax=1e-4, cmap=None):
+            vmax=1e-4, cmap=None, smooth=True):
     cbar = cbar or vmin is None or vmax is None
     img = imagesc(ax, data, alpha=alpha, vmin=vmin, vmax=vmax, origin='lower',
-                  cmap=cmap)
+                  cmap=cmap, smooth=smooth)
     if cbar:
         fig.colorbar(img, ax=ax)
     if title is not None:
