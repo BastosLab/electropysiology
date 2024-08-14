8a9b864fa77333d13fedc67cbd5fa5806c701d24
Eli Sennesh <eli.sennesh@vanderbilt.edu>
2024-08-14 13:34:16 -0400

    statistics.grand.GrandConcatenation: statistic to concatenate aligned trials
    Signed-off-by: Eli Sennesh <eli.sennesh@vanderbilt.edu>

--- epych/statistics/grand.py
+++ epych/statistics/grand.py
@@ -12,6 +12,62 @@ from . import alignment
 
 T = TypeVar('T', bound=signal.EpochedSignal)
 
+class GrandConcatenation(statistic.Statistic[T]):
+    def __init__(self, alignment: alignment.LaminarAlignment, data=None):
+        super().__init__((alignment.num_channels, alignment.num_times),
+                         data=data)
+        self._alignment = alignment
+        self._dt = None
+        if data is None:
+            self._data = {"channels": None, "k": 0, "cat": None,
+                          "timestamps": np.zeros(self.iid_shape[1])}
+        self._signal_class = None
+
+    @property
+    def alignment(self):
+        return self._alignment
+
+    def apply(self, element: T):
+        element = self.alignment.align(self.data["k"], element)
+        assert len(element.channels) == self.num_channels
+        assert element.data.shape[0] == self.num_channels
+        data = element.data
+        if self.num_times < element.data.shape[1]:
+            data = data[:, :self.num_times, :]
+        running = copy.deepcopy(self.data)
+
+        channels = element.channels.reset_index(drop=True)
+        if running["channels"] is None:
+            running["channels"] = channels
+            self._dt = element._dt
+            self._signal_class = element.__class__
+        else:
+            for column in running["channels"].columns:
+                if running["channels"][column].values.dtype == np.int64:
+                    running["channels"][column] += channels[column]
+
+        running["k"] += 1
+        running["cat"] = data if running["cat"] is None else\
+                         np.cat((running["cat"], data), axis=-1)
+        running["timestamps"] += element.times[:self.num_times]
+        return running
+
+    @property
+    def num_channels(self):
+        return self.iid_shape[0]
+
+    @property
+    def num_times(self):
+        return self.iid_shape[1]
+
+    def result(self):
+        times = self.data["timestamps"] / self.data["k"]
+        channels = self.data["channels"].copy()
+        for column in channels.columns:
+            if channels[column].values.dtype == np.int64:
+                channels[column] //= self.data["k"]
+        return self._signal_class(channels, self.data["cat"], self._dt, times)
+
 class GrandAverage(statistic.Statistic[T]):
     def __init__(self, alignment: alignment.LaminarAlignment, data=None):
         super().__init__((alignment.num_channels, alignment.num_times),
