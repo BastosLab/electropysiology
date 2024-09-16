aa4c933d997b91aed242f0410cb81847fadf96cb
Eli Sennesh <eli.sennesh@vanderbilt.edu>
2024-09-16 16:20:50 -0500

    statistics.spectrum.PowerSpectrum: use MNE to calculate signal PSD
    Signed-off-by: Eli Sennesh <eli.sennesh@vanderbilt.edu>

--- epych/statistics/spectrum.py
+++ epych/statistics/spectrum.py
@@ -5,7 +5,6 @@ import mne
 import numpy as np
 import quantities as pq
 import scipy.fft as fft
-import syncopy as spy
 
 from .. import plotting, signal, statistic
 
@@ -19,8 +18,7 @@ class PowerSpectrum(statistic.ChannelwiseStatistic[signal.EpochedSignal]):
             fmax = np.array(fmax) * pq.Hz
         self._df = df.rescale("Hz")
         self._f0 = f0.rescale("Hz")
-        self._freqs = np.arange(0, fmax.item(), df.item())
-        self._freqs = (self._freqs + df.item()) * df.units
+        self._fmax = fmax * pq.Hz
         self._taper = taper
         super().__init__(channels, (int((fmax / df).item()),), data=data)
 
@@ -33,22 +31,15 @@ class PowerSpectrum(statistic.ChannelwiseStatistic[signal.EpochedSignal]):
         xs = mne.EpochsArray(np.moveaxis(element.data.magnitude, -1, 0),
                              mne.create_info(channels, self.f0.item()),
                              tmin=element.times[0].item())
-        data = spy.mne_epochs_to_tldata(xs)
-        cfg = spy.get_defaults(spy.freqanalysis)
-        cfg.foi = self.freqs.magnitude.squeeze()
-        cfg.ft_compat = True
-        cfg.keeptrials = 'no'
-        cfg.method = 'mtmfft'
-        cfg.output = 'pow'
-        cfg.t_ftimwin = 0.25
-        cfg.taper = self._taper
-        cfg.tapsmofrq = 4
-        cfg.toi = "all"
-        psd = spy.freqanalysis(cfg, data).show()
+        psd = xs.compute_psd(fmax=GAMMA_BAND[1].item(), fmin=0.5,
+                             method="multitaper", n_jobs=-1, picks="all",
+                             remove_dc=True)
+        self._freqs = psd.freqs
+        psd = np.moveaxis(psd.get_data(picks="all"), 0, -1)
 
         if self.data is None:
-            return np.moveaxis(psd, 0, 1)[:, :, np.newaxis]
-        return np.concatenate((self.data, psd), axis=-1)
+            return psd
+        return np.concatenate((self.data["psd"], psd), axis=-1)
 
     def band_power(self, fbottom, ftop):
         ibot = np.nanargmin((self.freqs - fbottom) ** 2)
@@ -82,11 +73,14 @@ class PowerSpectrum(statistic.ChannelwiseStatistic[signal.EpochedSignal]):
 
     @property
     def fmax(self):
-        return self._freqs[-1]
+        return self._fmax
 
     @property
     def freqs(self):
-        return self._freqs
+        if hasattr(self, "_freqs"):
+            return self._freqs
+        freqs = np.arange(0, self.fmax.item(), self.df.item()) + self.df.item()
+        return freqs * self.df.units
 
     def heatmap(self, fbottom=0, ftop=None, ax=None, fig=None):
         if ax is None:
@@ -111,5 +105,4 @@ class PowerSpectrum(statistic.ChannelwiseStatistic[signal.EpochedSignal]):
         ax.invert_yaxis()
 
     def relative(self):
-        max_pow = self.data.max(axis=0, keepdims=True)
-        return self.fmap(lambda vals: vals / max_pow)
+        return self.fmap(lambda vals: vals / vals.max(axis=0, keepdims=True))
