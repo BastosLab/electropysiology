b774af89d6b7e2d775e2ad8344ccedd9cd51cf4b
Eli Sennesh <eli.sennesh@vanderbilt.edu>
2024-11-05 15:51:36 -0600

    signals.tfr.TimeFrequencyRepr: time-frequency representation Signals by channel
    Signed-off-by: Eli Sennesh <eli.sennesh@vanderbilt.edu>

---
+++ epych/signals/tfr.py
@@ -0,0 +1,67 @@
+#!/usr/bin/python3
+
+import dask.array
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import quantities as pq
+
+from .. import plotting, signal
+from ..statistics.spectrum import PowerSpectrum
+
+class TimeFrequencyRepr(signal.Signal):
+    def __init__(self, channels: pd.DataFrame, data, dt, freqs, timestamps):
+        self._freqs = freqs
+        super().__init__(channels, data, dt, timestamps)
+
+    def channel_depths(self, column=None):
+        if column is not None and column in self.channels:
+            return self.channels[column].values
+        return np.arange(len(self.channels))
+
+    @property
+    def freqs(self):
+        return self._freqs
+
+    def fmap(self, f):
+        return self.__class__(self.channels, f(self.data), self.dt, self.freqs,
+                              self.times)
+
+    @property
+    def fmax(self):
+        return self._freqs[-1]
+
+class EpochedTfr(TimeFrequencyRepr, signal.EpochedSignal):
+    def __init__(self, channels: pd.DataFrame, data, dt, freqs, timestamps):
+        assert len(data.shape) == 4
+        assert len(channels) == data.shape[0]
+        assert len(timestamps) == data.shape[1]
+        assert timestamps.units == dt.units
+
+        super(EpochedTfr, self).__init__(channels, data, dt, freqs, timestamps)
+
+    def evoked(self):
+        erp = super().evoked()
+        return EvokedTfr(erp.channels, erp.data, erp.dt, self.freqs, erp.times)
+
+class EvokedTfr(TimeFrequencyRepr, signal.EvokedSignal):
+    def __init__(self, channels: pd.DataFrame, data, dt, freqs, timestamps):
+        assert data.shape[-1] == 1
+        super(EvokedTfr, self).__init__(channels, data, dt, freqs, timestamps)
+
+    def evoked(self):
+        erp = super().evoked()
+        return EvokedTfr(erp.channels, erp.data, erp.dt, self.freqs, erp.times)
+
+    def plot(self, *args, **kwargs):
+        if "events" in kwargs:
+            events = kwargs.pop("events")
+            def callback(self, ax):
+                for (event, (time, color)) in events.items():
+                    ymin, ymax = ax.get_ybound()
+                    xtime = self.sample_at(time)
+                    ax.vlines(xtime, ymin, ymax, colors=color,
+                              linestyles='dashed', label=event)
+                    ax.annotate(event, (xtime + 0.005, ymax))
+            kwargs["callback"] = callback
+        return self.heatmap(*args, **kwargs)
