8afcb0378aa0dbcfb99dc31dd4e02cd2dcb61e0e
Eli Sennesh <eli.sennesh@vanderbilt.edu>
2024-11-15 18:27:50 -0600

    signals.tfr.EvokedTfr: annotate baseline time with a black highlight+caption
    Signed-off-by: Eli Sennesh <eli.sennesh@vanderbilt.edu>

--- epych/signals/tfr.py
+++ epych/signals/tfr.py
@@ -88,7 +88,7 @@ class EvokedTfr(TimeFrequencyRepr, signal.EvokedSignal):
 
     def heatmap(self, alpha=None, ax=None, cmap=None, fbottom=0, fig=None,
                 filename=None, ftop=None, title=None, vlim=None, vmin=None,
-                vmax=None, **events):
+                vmax=None, baseline=None, **events):
         lone = fig is None
         if fig is None:
             fig = plt.figure(figsize=(self.plot_width * 4, 3))
@@ -123,6 +123,12 @@ class EvokedTfr(TimeFrequencyRepr, signal.EvokedSignal):
         yticks[-1] = min(yticks[-1], tfrs.shape[-1] - 1)
         ax.set_yticks(yticks, ['{0:,.2f}'.format(f) for f in freqs[yticks]])
         ymin, ymax = ax.get_ybound()
+
+        if baseline is not None:
+            bxmin = self.sample_at(baseline[0])
+            bxmax = self.sample_at(baseline[1])
+            ax.axvspan(bxmin, bxmax, alpha=0.1, color='g')
+            ax.annotate("Baseline", (bxmin + 0.5, ymax - 1))
 
         for (event, (time, color)) in events.items():
             xtime = self.sample_at(time)
